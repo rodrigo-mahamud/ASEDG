da1027125750e04d162e57ede575d1d4464c3a05
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-07-02 18:43:45 +0200

    skeleton adjustments

--- src/components/BookingPayment.tsx
+++ src/components/BookingPayment.tsx
@@ -3,6 +3,7 @@ import { Elements } from '@stripe/react-stripe-js'
 import { stripePromise } from '@/utils/stripeUtils'
 import { BookingCheckout } from './BookingCheckout'
 import { Skeleton } from '@/components/lib/skeleton'
+import useFormStore from '@/utils/useBookingState'
 
 interface BookingPaymentProps {
   clientSecret: string | null
@@ -11,27 +12,44 @@ interface BookingPaymentProps {
 }
 
 export function BookingPayment({ clientSecret, onPaymentComplete, onError }: BookingPaymentProps) {
-  if (!clientSecret) {
-    return (
-      <div className="space-y-6 p-4">
+  const { isLoading } = useFormStore()
+
+  const renderSkeleton = () => (
+    <div className="space-y-5 p-4">
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-4 w-1/4" />
         <Skeleton className="h-10 w-full" />
-        <div className="flex gap-4">
-          <Skeleton className="h-10 w-full" />
+      </div>
+      <div className="flex gap-6">
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-4 w-1/4" />
           <Skeleton className="h-10 w-full" />
         </div>
-
-        <Skeleton className="h-10 w-full mt-4" />
+        <div className="flex flex-col gap-2 w-full">
+          <Skeleton className="h-4 w-1/4" />
+          <Skeleton className="h-10 w-full" />
+        </div>
       </div>
-    )
-  }
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-10 w-full" />
+      </div>
+    </div>
+  )
 
-  return (
-    <Elements stripe={stripePromise} options={{ clientSecret }}>
+  const renderStripeForm = () => (
+    <Elements stripe={stripePromise} options={{ clientSecret: clientSecret! }}>
       <BookingCheckout
         onPaymentComplete={onPaymentComplete}
         onError={onError}
-        clientSecret={clientSecret}
+        clientSecret={clientSecret!}
       />
     </Elements>
+  )
+
+  return (
+    <div className="min-h-[250px] transition-all duration-300">
+      {isLoading || !clientSecret ? renderSkeleton() : renderStripeForm()}
+    </div>
   )
 }
