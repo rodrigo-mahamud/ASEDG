ca5c701999a01b329a64cd2ae4eba272bf384167
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-07-09 00:04:08 +0200

    new confetti

--- src/components/BookingSuccess.tsx
+++ src/components/BookingSuccess.tsx
@@ -8,37 +8,38 @@ interface BookingSuccessProps {
 }
 
 export function BookingSuccess({ message }: BookingSuccessProps) {
-  const [isConfettiRunning, setIsConfettiRunning] = useState(true)
   const [play] = useSound('/success.mp3', { volume: 0.4 })
 
+  const handleClick = () => {
+    const duration = 4.5 * 1000
+    const animationEnd = Date.now() + duration
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+    const randomInRange = (min: number, max: number) => Math.random() * (max - min) + min
+
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now()
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval)
+      }
+
+      const particleCount = 50 * (timeLeft / duration)
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    }, 250)
+  }
   useEffect(() => {
-    const handleClick = () => {
-      const duration = 5 * 1000
-      const animationEnd = Date.now() + duration
-      const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
-
-      const randomInRange = (min: number, max: number) => Math.random() * (max - min) + min
-
-      const interval = window.setInterval(() => {
-        const timeLeft = animationEnd - Date.now()
-
-        if (timeLeft <= 0) {
-          return clearInterval(interval)
-        }
-
-        const particleCount = 50 * (timeLeft / duration)
-        confett({
-          ...defaults,
-          particleCount,
-          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-        })
-        confetti({
-          ...defaults,
-          particleCount,
-          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-        })
-      }, 250)
-    }
+    play()
+    handleClick()
   }, [play])
 
   return (
