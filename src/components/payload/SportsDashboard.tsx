6bebe75030e9a73ddd0b6839410f9fe9e02f7fab
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-07-14 20:55:29 +0200

    shadcn chart for SportsDashboard

--- src/components/payload/SportsDashboard.tsx
+++ src/components/payload/SportsDashboard.tsx
@@ -9,7 +9,7 @@ import { PieGraph } from './charts/pie-graph'
 export default function SportsDashboard() {
   return (
     <Gutter>
-      <div>
+      <div className="space-y-4">
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
