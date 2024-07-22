1f82ab8c3cde6afd980c0184535d104f4da1ed65
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-07-22 19:03:08 +0200

    delete dialog done

--- src/components/payload/table/DeleteVisitor.tsx
+++ src/components/payload/table/DeleteVisitor.tsx
@@ -11,9 +11,12 @@ import {
   AlertDialogTitle,
 } from '@/components/lib/alert-dialog'
 import { Button } from '@/components/lib/button'
-import { IconTrash } from '@tabler/icons-react'
+import { IconAlertCircle, IconTrash } from '@tabler/icons-react'
 import { FloatingLabelInput } from '@/components/lib/floatinglabel'
 import { useClientEditStore } from '@/utils/dashboard/dashboardStore'
+import { Alert, AlertDescription, AlertTitle } from '@/components/lib/alert'
+import { Input } from '@/components/lib/input'
+import { Label } from '@/components/lib/label'
 
 export function DeleteVisitor() {
   const [confirmText, setConfirmText] = useState('')
@@ -27,7 +30,7 @@ export function DeleteVisitor() {
   console.log(usersToDelete)
 
   const handleConfirm = () => {
-    if (confirmText === 'BORRAR') {
+    if (confirmText === 'eliminar-usuarios') {
       deleteSelectedClients()
       setConfirmText('')
       setIsDeleteDialogOpen(false)
@@ -36,39 +39,53 @@ export function DeleteVisitor() {
 
   return (
     <AlertDialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
-      <AlertDialogContent className="useTw border-border ">
-        <AlertDialogHeader>
-          <AlertDialogTitle className="useTw text-xl">
+      <AlertDialogContent className="useTw border-border gap-6 p-0">
+        <AlertDialogHeader className="px-6 pt-6">
+          <AlertDialogTitle className="useTw text-2xl font-bold">
             Eliminar {selectedClients.length === 1 ? 'Usuario' : 'Usuarios'}
           </AlertDialogTitle>
-          <AlertDialogDescription className="text-base text-white/75">
-            Estás seguro de que deseas eliminar a:
-            <ul className="list-disc pl-5 mt-2 max-h-64 overflow-y-auto">
+          <AlertDialogDescription className="text-base text-white/75 text-pretty">
+            Si continuas vas a
+            {selectedClients.length === 1
+              ? ' eliminar el siguiente usuario, '
+              : ` eliminar estos ${selectedClients.length} usuarios, `}
+            ¿estás seguro de que deseas
+            {selectedClients.length === 1 ? ' eliminarlo?' : ` continuar? `}
+            <ul className="pl-0 mt-4 mb-0 max-h-44 overflow-y-auto font-semibold text-white space-y-2">
               {usersToDelete.map((user) => (
                 <li key={user.id}>{user.name}</li>
               ))}
             </ul>
-            Se borrarán permanentemente {selectedClients.length}{' '}
-            {selectedClients.length === 1 ? 'visitante' : 'visitantes'} seleccionados de la base de
-            datos.
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <div className="py-4 bg-onTop ">
-          <FloatingLabelInput
-            label="Escribe BORRAR para confirmar"
+        <div className="bg-onTop p-6 border-y border-border">
+          <Alert variant="destructive" className="flex items-start p-3 mb-6">
+            <IconAlertCircle size={16} className="mr-2 mt-1 w-1/9" />
+            <AlertTitle className="mr-1 mb-0 text-base font-normal w-full">
+              <b>Atención:</b> Recuerda que esta acción es irreversible.
+            </AlertTitle>
+          </Alert>
+          <Label className="text-base font-normal">
+            Para verificar, escribe aqui: <b>eliminar-usuarios</b>
+          </Label>
+          <Input
             value={confirmText}
-            className="bg-transparent text-white/50"
+            className="bg-transparent text-white p-6 text-base mt-3"
             onChange={(e) => setConfirmText(e.target.value)}
-          />
+          ></Input>
         </div>
-        <AlertDialogFooter>
+        <AlertDialogFooter className="px-6 pb-6">
           <AlertDialogCancel
             onClick={() => setIsDeleteDialogOpen(false)}
             className="text-base rounded-md border-border"
           >
             Cancelar
           </AlertDialogCancel>
-          <AlertDialogAction onClick={handleConfirm} disabled={confirmText !== 'BORRAR'} asChild>
+          <AlertDialogAction
+            onClick={handleConfirm}
+            disabled={confirmText !== 'eliminar-usuarios'}
+            asChild
+          >
             <Button variant="destructive" className="flex items-center useTw text-base rounded-md">
               <IconTrash className="mr-1" stroke={1.5} size={16} />
               Borrar
