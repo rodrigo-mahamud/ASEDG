2f9b73f6f0d74ed7808ef2f743c6742d44ad6826
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-07-23 14:14:09 +0200

    colums with more data

--- src/components/payload/table/Columns.tsx
+++ src/components/payload/table/Columns.tsx
@@ -16,15 +16,15 @@ const getStatusColor = (status: string) => {
     case 'upcoming':
       return 'bg-blue-600/50 text-blue-100 border-blue-600'
     case 'active':
-      return 'bg-green-600/50 text-green-100 border-green-600'
+      return 'bg-green-600/20 text-green-100/90 border-green-600/30'
     case 'visiting':
       return 'bg-green-600/50 text-green-100 border-green-600'
     case 'completed':
-      return 'bg-red-600/50 text-red-100 border-red-600'
+      return 'bg-red-600/20 text-red-100 border-red-600/30'
     case 'no_visit':
-      return 'bg-red-600/50 text-red-100 border-red-600'
+      return 'bg-red-600/20 text-red-100 border-red-600/30'
     case 'visited':
-      return 'bg-red-600/50 text-red-100 border-red-600'
+      return 'bg-red-600/20 text-red-100 border-red-600/30'
     default:
       return 'bg-yellow-600/50 text-yellow-100 border-yellow-600'
   }
@@ -48,7 +48,14 @@ const getStatusIcon = (status: string) => {
       return <IconCheck />
   }
 }
-
+const parseRemarks = (remarks: string): { age: string; dni: string; acceptedTerms: boolean } => {
+  const [age, dni, terms] = remarks.split(';')
+  return {
+    age,
+    dni,
+    acceptedTerms: terms === '1',
+  }
+}
 export const columns: ColumnDef<Visitor>[] = [
   {
     accessorFn: (row) => `${row.first_name} ${row.last_name}`,
@@ -61,7 +68,7 @@ export const columns: ColumnDef<Visitor>[] = [
           <Avatar>
             <AvatarFallback className="font-semibold">{initials}</AvatarFallback>
           </Avatar>
-          <h2 className="text-base font-semibold useTw">{fullName}</h2>
+          <h2 className="text-base font-semibold line-clamp-1 useTw">{fullName}</h2>
         </div>
       )
     },
@@ -98,6 +105,18 @@ export const columns: ColumnDef<Visitor>[] = [
   {
     accessorKey: 'email',
     header: 'Email',
+    cell: ({ row }) => {
+      const email = row.getValue('email') as string
+      return (
+        <a href={`mailto:${email}`} target="_blank" rel="nofollow noopener">
+          {email}
+        </a>
+      )
+    },
+  },
+  {
+    accessorKey: 'mobile_phone',
+    header: 'Teléfono de contacto',
   },
   {
     accessorKey: 'start_time',
@@ -115,6 +134,40 @@ export const columns: ColumnDef<Visitor>[] = [
       const timestamp = row.getValue('end_time') as number
       const date = dayjs(timestamp * 1000)
       return <div>{date.format('D, MMMM, YYYY')}</div>
+    },
+  },
+  {
+    accessorFn: (row) => parseRemarks(row.remarks).dni,
+    id: 'dni',
+    header: 'DNI',
+    cell: ({ row }) => {
+      const remarks = row.original.remarks
+      const { dni } = parseRemarks(remarks)
+      return <div>{dni}</div>
+    },
+  },
+  {
+    accessorFn: (row) => parseRemarks(row.remarks).age,
+    id: 'age',
+    header: 'Edad',
+    cell: ({ row }) => {
+      const remarks = row.original.remarks
+      const { age } = parseRemarks(remarks)
+      return <div>{age} Años</div>
+    },
+  },
+  {
+    accessorFn: (row) => parseRemarks(row.remarks).acceptedTerms,
+    id: 'acceptedTerms',
+    header: 'Términos',
+    cell: ({ row }) => {
+      const remarks = row.original.remarks
+      const { acceptedTerms } = parseRemarks(remarks)
+      return (
+        <Badge variant={acceptedTerms ? 'success' : 'destructive'}>
+          {acceptedTerms ? 'Aceptados' : 'No aceptados'}
+        </Badge>
+      )
     },
   },
   {
