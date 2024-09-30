15929886bcfa1dcfb719c0d2cae36bef3cc69113
Rodrigo Mahamud <rodrigomahamud@gmail.com>
2024-09-30 20:40:35 +0200

    newspage with metadata

--- src/app/(frontend)/noticias-san-esteban-de-gormaz/page.tsx
+++ src/app/(frontend)/noticias-san-esteban-de-gormaz/page.tsx
@@ -5,27 +5,66 @@ import RenderBlocks from '@/components/RenderBlocks'
 import Hero from '@/components/hero/Hero'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
+async function getPageData() {
+  const payload = await getPayloadHMR({ config: configPromise })
+  const indexPage = await payload.find({
+    collection: 'pages',
+    where: {
+      'header.pagetype': {
+        equals: 'newsPage',
+      },
+    },
+  })
+  const settings = await payload.findGlobal({
+    slug: 'settings',
+  })
+  console.log(settings)
 
-export async function generateStaticParams() {
-  return [{}]
+  if (!indexPage || indexPage.docs.length === 0) {
+    notFound()
+  }
+
+  return indexPage.docs[0]
+}
+async function getSettings() {
+  const payload = await getPayloadHMR({ config: configPromise })
+  const settings = await payload.findGlobal({
+    slug: 'settings',
+  })
+
+  return settings
+}
+export async function generateMetadata() {
+  const data = await getPageData()
+  console.log(data)
+
+  const seoData = data.meta || ({} as any)
+  const settings = await getSettings()
+
+  return {
+    title: seoData.title,
+    description: seoData.description,
+    openGraph: {
+      locale: 'es_ES',
+      title: seoData.title || settings.defaultTitle,
+      siteName: settings.defaultTitle,
+      url: `https://${process.env.ROOT_DOMAIN}/noticias-san-esteban-de-gormaz`,
+      description: seoData.description || settings.defaultDescription,
+      images: seoData?.image?.url || settings.defaultOgImage,
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      domain: process.env.ROOT_DOMAIN,
+      title: seoData.title || settings.defaultTitle,
+      description: seoData.description || settings.defaultDescription,
+      images: seoData?.image?.url || settings.defaultOgImage,
+    },
+  }
 }
 export default async function NewsMainPage() {
   try {
-    const payload = await getPayloadHMR({ config: configPromise })
-    const newsPage = await payload.find({
-      collection: 'pages',
-      where: {
-        'header.pagetype': {
-          equals: 'newsPage',
-        },
-      },
-    })
-
-    if (!newsPage) {
-      notFound()
-    }
-
-    const data = newsPage.docs[0]
+    const data = await getPageData()
 
     return (
       <main>
